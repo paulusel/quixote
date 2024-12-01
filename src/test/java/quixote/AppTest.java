42d374ead4bc098e5d42f8cff0353bb9fff6e485
paulusel <paulusel@proton.me>
2024-12-01 12:58:41 +0300

    build: Add tests

---
+++ src/test/java/quixote/AppTest.java
@@ -0,0 +1,19 @@
+package quixote;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
