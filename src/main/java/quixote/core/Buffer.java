3422a4c69430f0b7a9579929343c254e21c0c583
paulusel <paulusel@proton.me>
2024-12-04 20:44:33 +0300

    Minor changes to text buffer

--- src/main/java/quixote/core/Buffer.java
+++ src/main/java/quixote/core/Buffer.java
@@ -1,6 +1,9 @@
 package quixote.core;
 
+import io.qt.gui.QTextDocument;
+
 public class Buffer {
+    private QTextDocument document;
     public Buffer getLast(){
         return new Buffer();
     }
